5d932aa1091216adb35f79e6d3544821c236e897
fupinpinfu <tchthai@gmail.com>
2020-09-11 13:37:28 +0700

    initial commit

---
+++ js/cookiejs.js
@@ -0,0 +1,46 @@
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+convertdateToddMMyyyy = function(dDate) 
+{
+    var today = new Date(dDate);
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = dd + '/' + mm + '/' + yyyy;
+
+    return today;
+}
+
+convertdateToyyyymmdd = function(dDate)
+{
+    var today = new Date(dDate);
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = yyyy + '-' + mm + '-' + dd;
+
+    return today;
+}
---
+++ js/firebaseconfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDOXFKM8FWObwsFbyqA6dqnQGTNfQDLGUo",
+    authDomain: "bbhtime-fa236.firebaseapp.com",
+    databaseURL: "https://bbhtime-fa236.firebaseio.com",
+    projectId: "bbhtime-fa236",
+    storageBucket: "bbhtime-fa236.appspot.com",
+    messagingSenderId: "351132412421",
+    appId: "1:351132412421:web:82938d9c881c1908e8ca6c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
