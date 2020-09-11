1d677086a26d8101181612c97d24978e95b9951b
fupinpinfu <tchthai@gmail.com>
2020-09-11 16:46:13 +0700

    Rem getall and get where

--- js/app.js
+++ js/app.js
@@ -2,27 +2,27 @@
 const db = firebase.firestore();
 // console.log(db.collection('User'))
 
-//get all
-db.collection("User").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        console.log(doc.data());
-    });
-})
-.catch(function(error) {
-    console.log("Error getting documents: ", error);
-});
+// //get all
+// db.collection("User").get().then((querySnapshot) => {
+//     querySnapshot.forEach((doc) => {
+//         console.log(doc.data());
+//     });
+// })
+// .catch(function(error) {
+//     console.log("Error getting documents: ", error);
+// });
 
-// //get where 
-db.collection("User").where("userid","==",2)
-.get()
-.then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        console.log(doc.data());
-    });
-})
-.catch(function(error) {
-    console.log("Error getting documents: ", error);
-});
+// // //get where 
+// db.collection("User").where("userid","==",22) 
+// .get()
+// .then((querySnapshot) => {
+//     querySnapshot.forEach((doc) => {
+//         console.log(doc.data());
+//     });
+// })
+// .catch(function(error) {
+//     console.log("Error getting documents: ", error);
+// });
 
 insertuser = () =>
     {
